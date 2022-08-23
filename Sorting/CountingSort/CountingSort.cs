aa29f508912d5179cd0e2fed4fe5169aa2692fe3
dheerajdhall27 <dheerajdhall2709@gmail.com>
2022-08-23 11:09:55 -0400

    Added Counting Sort

---
+++ Sorting/CountingSort/CountingSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.CountingSort;
+
+internal class CountingSort
+{
+    public void Sort(int[] data)
+    {
+        int max = Int32.MinValue;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] > max)
+            { 
+                max = data[i];
+            }
+        }
+
+        int[] countArr = new int[max + 1];
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            countArr[data[i]] = countArr[data[i]] + 1;
+        }
+
+        for (int i = 1; i < countArr.Length; i++)
+        {
+            countArr[i] = countArr[i] + countArr[i - 1];
+        }
+
+
+        int[] resultArr = new int[data.Length];
+        for (int i = data.Length - 1; i >= 0; i--)
+        {
+            resultArr[countArr[data[i]] - 1] = data[i];
+            countArr[data[i]] = countArr[data[i]] - 1;
+        }
+
+        Array.Copy(resultArr, data, data.Length);
+    }
+}
+
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,10 +1,11 @@
 ﻿
 
+using Sorting.CountingSort;
 using Sorting.QuickSort;
 
-var quickSort = new QuickSort();
+var countingSort = new CountingSort();
 
-int[] arr = new int[] { 5, 4, 3, 2, 1 };
-quickSort.Sort(arr, 0, arr.Length - 1);
+int[] arr = new int[] {0, 5, 4, 3, 2, 1, 0 };
+countingSort.Sort(arr);
 
 Console.WriteLine(arr);
